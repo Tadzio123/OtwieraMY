ee615ab9f10ce74ce6e4745ffa8866c76d29cf52
JakubCywinski <jakubwebdev@gmail.com>
2021-01-30 19:23:42 +0100

    Update Icon Component

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -3,6 +3,10 @@ import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import { ReactComponent as PencilIcon } from 'assets/icons/edit.svg';
 import { ReactComponent as UnionIcon } from 'assets/icons/delete.svg';
+import { ReactComponent as SettingsIcon } from 'assets/icons/settings.svg';
+import { ReactComponent as GPSIcon } from 'assets/icons/gps.svg';
+import { ReactComponent as ArrowUpIcon } from 'assets/icons/arrowUp.svg';
+import { ReactComponent as ArrowDownIcon } from 'assets/icons/arrowDown.svg';
 
 const handleIconType = (name) => {
   switch (name) {
@@ -10,54 +14,57 @@ const handleIconType = (name) => {
       return <PencilIcon />;
     case 'union':
       return <UnionIcon />;
+    case 'settings':
+      return <SettingsIcon />;
+    case 'gps':
+      return <GPSIcon />;
+    case 'arrowUp':
+      return <ArrowUpIcon />;
+    case 'arrowDown':
+      return <ArrowDownIcon />;
     default:
       return <PencilIcon />;
-  }
-};
-
-const handleIconColor = (color) => {
-  switch (color) {
-    case 'green':
-      return css`
-        fill: ${({ theme }) => theme.colorSuccess};
-      `;
-    case 'red':
-      return css`
-        fill: ${({ theme }) => theme.colorDanger};
-      `;
-    default:
-      return css`
-        fill: ${({ theme }) => theme.colorBlack};
-      `;
   }
 };
 
 const StyledIcon = styled.div`
   align-self: flex-start;
   margin: 1.5rem 0 0 0;
+  svg {
+    height: ${({ height }) => height};
+    width: ${({ width }) => width};
+  }
   svg path {
-    ${({ color }) => handleIconColor(color)};
+    fill: ${({ color }) => color};
   }
 `;
 
 const Icon = ({
-  name, color,
+  name, color, width, height,
 }) => (
   <>
-    <StyledIcon color={color}>
+    <StyledIcon color={color} width={width} height={height}>
       {handleIconType(name)}
     </StyledIcon>
   </>
 );
 
 Icon.propTypes = {
-  name: PropTypes.oneOf(['pencil', 'union']),
-  color: PropTypes.oneOf(['green', 'red']),
+  name: PropTypes.oneOf(['pencil', 'union', 'settings', 'gps', 'arrowUp', 'arrowDown']),
+  color: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.func,
+  ]),
+  width: PropTypes.string,
+  height: PropTypes.string,
 };
 
 Icon.defaultProps = {
   name: 'union',
   color: ({ theme }) => theme.colorBlack,
+  width: '2rem',
+  height: '2rem',
 };
 
 export default Icon;
