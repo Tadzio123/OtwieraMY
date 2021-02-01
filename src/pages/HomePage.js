046f2b214c3f57f36ad33a53c5a25c864cd64a72
Tadzio123 <tadzio806@gmail.com>
2021-02-01 13:49:04 +0100

    remove unused imoports

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import SwitchedButton from 'components/molecules/SwitchedButton';
 import Nav from 'components/atoms/Nav';
-import LayerProfile from 'components/molecules/LayerProfile';
-import Dropdown from 'components/atoms/Dropdown';
 
 const HomePage = () => (
   <>
     <Nav>
       <SwitchedButton firstButtonText="Mapa" firstButtonFunc={() => {}} secondButtonText="Prawnicy" secondButtonFunc={() => {}} />
     </Nav>
-    <Dropdown />
   </>
 );
 
