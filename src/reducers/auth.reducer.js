7e68b2eaf8aee3f9045cd3e4dad29065fbf5017b
Tadzio123 <tadzio806@gmail.com>
2021-03-06 11:45:47 +0100

    auth fix

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -8,17 +8,15 @@ const authReducer = (state = initialState, action) => {
   switch (action.type) {
     case CONSTANTS.USER_LOGIN:
       return {
-        ...state,
         userLogged: true,
       };
     case CONSTANTS.USER_LOGOUT:
       return {
-        ...state,
         userLogged: false,
       };
     default:
       return {
-        state,
+        ...state,
       };
   }
 };
