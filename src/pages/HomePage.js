faba537a914f44742410c90ddef693ae236ce836
JakubCywinski <jakubwebdev@gmail.com>
2021-03-08 20:14:45 +0100

    Icon fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Map from 'components/organisms/Map';
 import { withRouter } from 'react-router-dom';
-import Alert from 'components/molecules/Alerts';
+import Menu from 'components/molecules/Menu';
 
 const HomePage = () => (
   <>
     <Map />
-    <Alert type="success" message="Wow ale super" />
+    <Menu />
   </>
 );
 
