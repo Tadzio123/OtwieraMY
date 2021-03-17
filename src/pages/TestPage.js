d191883ce45ee247b161491f8b1f2ad9ea6a58e0
JakubCywinski <jakubwebdev@gmail.com>
2021-03-17 04:52:07 +0100

    Added geolocation event to test (TestPage)

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import mapActions from 'actions/map.actions';
@@ -15,11 +15,49 @@ const Container = styled.div`
 `;
 
 // eslint-disable-next-line react/prop-types
-const TestPage = () => (
-	<Container>
-		<Modal isOpen='true' />
-	</Container>
-);
+const TestPage = () => {
+	useEffect(() => {
+		function geoFindMe() {
+			const status = document.querySelector('#status');
+			const mapLink = document.querySelector('#map-link');
+
+			mapLink.href = '';
+			mapLink.textContent = '';
+
+			function success(position) {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+
+				status.textContent = '';
+				// mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+				mapLink.href = `https://www.google.pl/maps/@${latitude},${longitude}`;
+				mapLink.textContent = `X: ${latitude} °, Y: ${longitude} °`;
+			}
+
+			function error() {
+				status.textContent = 'Nie możemy cię zlokalizować';
+			}
+
+			if (!navigator.geolocation) {
+				status.textContent = 'Twoja przeglądarka nie wspiera geolokalizacji';
+			} else {
+				status.textContent = 'Locating…';
+				navigator.geolocation.getCurrentPosition(success, error);
+			}
+		}
+
+		document.querySelector('#find-me').addEventListener('click', geoFindMe);
+	});
+
+	return (
+		<Container>
+			<button id="find-me">Show my location</button>
+			<br />
+			<p id="status"></p>
+			<a id="map-link" target="_blank"></a>
+		</Container>
+	);
+};
 const mapStateToProps = (state) => ({
 	activeMarker: state.markerID.selectedMarkerID,
 	activeMarkerData: state.markerData.selectedMarkerData,
