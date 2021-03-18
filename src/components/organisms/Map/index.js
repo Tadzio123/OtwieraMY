2cf76cab28d6308a6baaba571c0ce805aa38d18d
JakubCywinski <jakubwebdev@gmail.com>
2021-03-18 15:35:44 +0100

    Layout Fixes

--- src/components/organisms/Map/index.js
+++ src/components/organisms/Map/index.js
@@ -1,7 +1,6 @@
+/* eslint-disable */
 import React, { useState, useEffect } from 'react';
-import {
-  MapContainer, TileLayer, Marker, useMap,
-} from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import styled from 'styled-components';
 import placeService from 'services/places.service';
 import MarkerIcon from 'components/molecules/MarkerIcon';
@@ -12,138 +11,149 @@ import mapActions from 'actions/map.actions';
 import { useAlert } from 'react-alert';
 
 const StyledMapContainer = styled(MapContainer)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  
-  width: 100%;
-  height: 100vh;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: 1;
+
+	width: 100%;
+	height: 100vh;
 `;
 
 const UserLocationMarker = ({ userLocation }) => {
-  const map = useMap();
+	const map = useMap();
 
-  return (
-    <>
-      {userLocation && (
-        <Marker
-          position={userLocation}
-          icon={MarkerIcon.user}
-          id="user-location-marker"
-          eventHandlers={{
-            click: map.setView(userLocation, 10),
-          }}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			{userLocation && (
+				<Marker
+					position={userLocation}
+					icon={MarkerIcon.user}
+					id="user-location-marker"
+					eventHandlers={{
+						click: map.setView(userLocation, 10),
+					}}
+				/>
+			)}
+		</>
+	);
 };
 
 const Map = ({
-  activeMarker, activeMarkerData, setActiveMarker, setActiveMarkerData, userLocation,
+	activeMarker,
+	activeMarkerData,
+	setActiveMarker,
+	setActiveMarkerData,
+	userLocation,
 }) => {
-  const [coordinates, setCoordinates] = useState([]);
-  const alert = useAlert();
+	const [coordinates, setCoordinates] = useState([]);
+	const alert = useAlert();
 
-  const getAllPlacesCoordinates = async () => {
-    await placeService
-      .getAllPlacesCoordinates()
-      .then((req) => req.json())
-      .then((json) => setCoordinates(json))
-      .catch(() => {
-        alert.error('Coś poszło nie tak');
-      });
-  };
+	const getAllPlacesCoordinates = async () => {
+		await placeService
+			.getAllPlacesCoordinates()
+			.then((req) => req.json())
+			.then((json) => setCoordinates(json))
+			.catch(() => {
+				alert.error('Coś poszło nie tak');
+			});
+	};
 
-  useEffect(() => {
-    getAllPlacesCoordinates();
-  }, []);
+	useEffect(() => {
+		getAllPlacesCoordinates();
+	}, []);
 
-  const defaultMapPosition = () => {
-    if (userLocation !== null) {
-      return userLocation;
-    }
-    return [51.919437, 19.145136];
-  };
+	const defaultMapPosition = () => {
+		if (userLocation !== null) {
+			return userLocation;
+		}
+		return [51.919437, 19.145136];
+	};
 
-  // set icon to active marker
-  const getMarkerIcon = (index) => {
-    if (index === activeMarker) {
-      return MarkerIcon.primaryActive;
-    }
-    return MarkerIcon.primary;
-  };
+	// set icon to active marker
+	const getMarkerIcon = (index) => {
+		if (index === activeMarker) {
+			return MarkerIcon.primaryActive;
+		}
+		return MarkerIcon.primary;
+	};
 
-  // set marker active and get data from endpoint by marker id
-  const setActiveMarkerFunc = async (id) => {
-    setActiveMarker(id);
-    await placeService.getPlaceByCoordinate(id)
-      .then((res) => (res.json()))
-      .then((json) => setActiveMarkerData(json))
-      .catch(() => {
-        alert.error('Coś poszło nie tak');
-      });
-  };
+	// set marker active and get data from endpoint by marker id
+	const setActiveMarkerFunc = async (id) => {
+		setActiveMarker(id);
+		await placeService
+			.getPlaceByCoordinate(id)
+			.then((res) => res.json())
+			.then((json) => setActiveMarkerData(json))
+			.catch(() => {
+				alert.error('Coś poszło nie tak');
+			});
+	};
 
-  return (
-    <>
-      <StyledMapContainer center={defaultMapPosition()} zoom={10} scrollWheelZoom zoomControl={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://api.maptiler.com/maps/bright/{z}/{x}/{y}.png?key=ABxrBA7sOVSSwxg7OTjT"
-        />
+	return (
+		<>
+			<StyledMapContainer
+				center={defaultMapPosition()}
+				zoom={10}
+				scrollWheelZoom
+				zoomControl={false}
+			>
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://api.maptiler.com/maps/bright/{z}/{x}/{y}.png?key=ABxrBA7sOVSSwxg7OTjT"
+				/>
 
-        <UserLocationMarker userLocation={userLocation} />
+				<UserLocationMarker userLocation={userLocation} />
 
-        {coordinates.map((place) => (
-          <Marker
-            key={place.id}
-            index={place.id}
-            icon={getMarkerIcon(place.id)}
-            position={[place.coordinateX, place.coordinateY]}
-            eventHandlers={{
-              click: () => setActiveMarkerFunc(place.id),
-            }}
-          />
-        ))}
-        {activeMarkerData && (
-        <StyledPopup
-          position={[
-            activeMarkerData.coordinateX,
-            activeMarkerData.coordinateY,
-          ]}
-          onClose={() => {
-            setActiveMarkerData(null);
-            setActiveMarker(null);
-          }}
-        >
-          <Typography component="h4" type="font-md-regular">{activeMarkerData.name}</Typography>
-          <Typography component="p" type="font-sm-light">
-            {activeMarkerData.street}
-            {' '}
-            {activeMarkerData.number}
-            {', '}
-            {activeMarkerData.postalCode}
-            {' '}
-            {activeMarkerData.city}
-          </Typography>
-        </StyledPopup>
-        )}
-      </StyledMapContainer>
-    </>
-  );
+				{coordinates.map((place) => (
+					<Marker
+						key={place.id}
+						index={place.id}
+						icon={getMarkerIcon(place.id)}
+						position={[place.coordinateX, place.coordinateY]}
+						eventHandlers={{
+							click: () => {
+								setActiveMarkerFunc(place.id);
+								map.setView([place.coordinateX, place.coordinateY], 10);
+							},
+						}}
+					/>
+				))}
+				{activeMarkerData && (
+					<StyledPopup
+						position={[
+							activeMarkerData.coordinateX,
+							activeMarkerData.coordinateY,
+						]}
+						onClose={() => {
+							setActiveMarkerData(null);
+							setActiveMarker(null);
+						}}
+					>
+						<Typography component="h4" type="font-md-regular">
+							{activeMarkerData.name}
+						</Typography>
+						<Typography component="p" type="font-sm-light">
+							{activeMarkerData.street} {activeMarkerData.number}
+							{', '}
+							{activeMarkerData.postalCode} {activeMarkerData.city}
+						</Typography>
+					</StyledPopup>
+				)}
+			</StyledMapContainer>
+		</>
+	);
 };
 
 const mapStateToProps = (state) => ({
-  activeMarker: state.markerID.selectedMarkerID,
-  activeMarkerData: state.markerData.selectedMarkerData,
-  userLocation: state.userLocation.userLocationData,
+	activeMarker: state.markerID.selectedMarkerID,
+	activeMarkerData: state.markerData.selectedMarkerData,
+	userLocation: state.userLocation.userLocationData,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setActiveMarker: (id) => dispatch(mapActions.activeMarkerID(id)),
-  setActiveMarkerData: (data) => dispatch(mapActions.activeMarkerData(data)),
+	setActiveMarker: (id) => dispatch(mapActions.activeMarkerID(id)),
+	setActiveMarkerData: (data) => dispatch(mapActions.activeMarkerData(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Map);
