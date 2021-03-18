9d1e52c979fe312fe419c3f49e35f17f3cc0ab76
Tadzio123 <tadzio806@gmail.com>
2021-03-18 09:31:52 +0100

    Fix popup position

--- src/components/molecules/MarkerIcon/index.js
+++ src/components/molecules/MarkerIcon/index.js
@@ -1,6 +1,7 @@
 import L from 'leaflet';
 import markerPrimaryIcon from 'assets/icons/marker-primary.svg';
 import markerPrimaryActiveIcon from 'assets/icons/marker-primary-active.svg';
+import markerUserIcon from 'assets/icons/marker-user.svg';
 
 const sizeOfIcon = [35, 50];
 
@@ -14,9 +15,15 @@ const markerPrimaryActive = new L.Icon({
   iconSize: sizeOfIcon,
 });
 
+const markerUser = new L.Icon({
+  iconUrl: markerUserIcon,
+  iconSize: [12.5, 12.5],
+});
+
 const MarkerIcon = {
   primary: markerPrimary,
   primaryActive: markerPrimaryActive,
+  user: markerUser,
 };
 
 export default MarkerIcon;
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,71 +1,69 @@
-/* eslint-disable */
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import mapActions from 'actions/map.actions';
-import ConfirmationModal from 'components/atoms/ConfirmationModal';
-import Modal from 'components/atoms/Modal';
 
 const Container = styled.div`
-	width: 100%;
-	height: 100vh;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-// eslint-disable-next-line react/prop-types
 const TestPage = () => {
-	useEffect(() => {
-		function geoFindMe() {
-			const status = document.querySelector('#status');
-			const mapLink = document.querySelector('#map-link');
+  useEffect(() => {
+    function geoFindMe() {
+      const status = document.querySelector('#status');
+      const mapLink = document.querySelector('#map-link');
 
-			mapLink.href = '';
-			mapLink.textContent = '';
+      mapLink.href = '';
+      mapLink.textContent = '';
 
-			function success(position) {
-				const latitude = position.coords.latitude;
-				const longitude = position.coords.longitude;
+      function success(position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
 
-				status.textContent = '';
-				// mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-				mapLink.href = `https://www.google.pl/maps/@${latitude},${longitude}`;
-				mapLink.textContent = `X: ${latitude} °, Y: ${longitude} °`;
-			}
+        status.textContent = '';
+        // mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.href = `https://www.google.pl/maps/@${latitude},${longitude}`;
+        mapLink.textContent = `X: ${latitude} °, Y: ${longitude} °`;
+      }
 
-			function error() {
-				status.textContent = 'Nie możemy cię zlokalizować';
-			}
+      function error() {
+        status.textContent = 'Nie możemy cię zlokalizować';
+      }
 
-			if (!navigator.geolocation) {
-				status.textContent = 'Twoja przeglądarka nie wspiera geolokalizacji';
-			} else {
-				status.textContent = 'Locating…';
-				navigator.geolocation.getCurrentPosition(success, error);
-			}
-		}
+      if (!navigator.geolocation) {
+        status.textContent = 'Twoja przeglądarka nie wspiera geolokalizacji';
+      } else {
+        status.textContent = 'Locating…';
+        navigator.geolocation.getCurrentPosition(success, error);
+      }
+    }
 
-		document.querySelector('#find-me').addEventListener('click', geoFindMe);
-	});
+    document.querySelector('#find-me').addEventListener('click', geoFindMe);
+  });
 
-	return (
-		<Container>
-			<button id="find-me">Show my location</button>
-			<br />
-			<p id="status"></p>
-			<a id="map-link" target="_blank"></a>
-		</Container>
-	);
+  return (
+    <Container>
+      <button id="find-me" type="button">Show my location</button>
+      <br />
+      <p id="status" />
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+      <a id="map-link" target="_blank" href="#">Link</a>
+    </Container>
+  );
 };
+
 const mapStateToProps = (state) => ({
-	activeMarker: state.markerID.selectedMarkerID,
-	activeMarkerData: state.markerData.selectedMarkerData,
+  activeMarker: state.markerID.selectedMarkerID,
+  activeMarkerData: state.markerData.selectedMarkerData,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	setActiveMarker: (id) => dispatch(mapActions.activeMarkerID(id)),
-	setActiveMarkerData: (data) => dispatch(mapActions.activeMarkerData(data)),
+  setActiveMarker: (id) => dispatch(mapActions.activeMarkerID(id)),
+  setActiveMarkerData: (data) => dispatch(mapActions.activeMarkerData(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TestPage);
