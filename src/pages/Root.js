9fadc74b02af391ebeb2a658e95ea40587b1a005
Tadzio123 <tadzio806@gmail.com>
2021-03-22 18:25:13 +0100

    Small fixes part 2

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,11 +1,10 @@
-/* eslint-disable */
 import React from 'react';
 
 import GlobalTemplate from 'template/GlobalTemplate';
 import LoginPage from 'pages/LoginPage';
 import HomePage from 'pages/HomePage';
 
-import { Switch, Route, Router, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import history from 'utils/history';
 import routes from 'utils/routes';
 
@@ -20,32 +19,32 @@ import store from '_helpers/store';
 import NotFound from './NotFound';
 
 const options = {
-	position: positions.TOP_CENTER,
-	transition: transitions.SCALE,
-	timeout: 5000,
-	offset: '0',
+  position: positions.TOP_CENTER,
+  transition: transitions.SCALE,
+  timeout: 5000,
+  offset: '0',
 };
 
 const Root = () => (
-	<Provider store={store}>
-		<ThemeProvider theme={theme}>
-			<ThemeContext.Provider value={theme}>
-				<AlertProvider template={Alert} {...options}>
-					<GlobalTemplate>
-							<Router history={history}>
-								<Switch>
-									<Route exact path={routes.login} component={LoginPage} />
-									<HomePageTemplate>
-										<Route exact path={routes.home} component={HomePage} />
-										<Route component={NotFound} />
-									</HomePageTemplate>
-								</Switch>
-							</Router>
-					</GlobalTemplate>
-				</AlertProvider>
-			</ThemeContext.Provider>
-		</ThemeProvider>
-	</Provider>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <ThemeContext.Provider value={theme}>
+        <AlertProvider template={Alert} {...options}>
+          <GlobalTemplate>
+            <Router history={history}>
+              <Switch>
+                <Route exact path={routes.login} component={LoginPage} />
+                <HomePageTemplate>
+                  <Route exact path={routes.home} component={HomePage} />
+                  <Route component={NotFound} />
+                </HomePageTemplate>
+              </Switch>
+            </Router>
+          </GlobalTemplate>
+        </AlertProvider>
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  </Provider>
 );
 
 export default Root;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
 const CONSTANTS = {
-	API_URL: 'https://otwieramygastro.pl:8888',
-	ACTIVE_MARKER_ID: 'ACTIVE_MARKER_ID',
-	ACTIVE_MARKER_DATA: 'ACTIVE_MARKER_DATA',
-	USER_LOCATION: 'USER_POSITION',
+  API_URL: 'https://otwieramygastro.pl:8889',
+  ACTIVE_MARKER_ID: 'ACTIVE_MARKER_ID',
+  ACTIVE_MARKER_DATA: 'ACTIVE_MARKER_DATA',
+  USER_LOCATION: 'USER_POSITION',
 };
 
 export default CONSTANTS;
